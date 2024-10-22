ec2737b5d99047d9ca299977884e58477a754b9d
米利昊 <milihao@zhihuics.com.cn>
2024-10-22 17:55:13 +0800

    [新增]优化开锁日志，授权日志，同步日志根据时间查询

--- Lock-admin/src/main/java/com/ruoyi/web/domain/vo/equipment/LockEquipmentParamVO.java
+++ Lock-admin/src/main/java/com/ruoyi/web/domain/vo/equipment/LockEquipmentParamVO.java
@@ -1,6 +1,8 @@
 package com.ruoyi.web.domain.vo.equipment;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ruoyi.web.domain.vo.PageVO;
+import java.util.Date;
 import lombok.Data;
 
 @Data
@@ -16,8 +18,9 @@ public class LockEquipmentParamVO extends PageVO {
     private Integer equipmentTypeId;
     private Integer equipmentModelId;
     private String description;
-
     private Integer activeDefenseFlag;
-
-
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date queryStartDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date queryEndDate;
 }
